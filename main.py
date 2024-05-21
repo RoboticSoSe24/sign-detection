e7ace522f8cd511b041604926bf981c7b6537f24
BenWeckend <ben.weckend@web.de>
2024-05-21 15:29:12 +0200

    new data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 
 train_dir = '/home/ben/code/schilderkennung/data'
 
-img_size = (96, 80)
+img_size = (150, 150)
 batch_size = 32
 
 train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
@@ -30,7 +30,7 @@ validation_generator = tf.keras.preprocessing.image.ImageDataGenerator(
 )
 
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(96, 80, 3)),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
